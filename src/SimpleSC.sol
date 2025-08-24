0fd92035762e9c898bf61cfe8b1155d209e418f8
cryptoeconprof <161665493+cryptoeconprof@users.noreply.github.com>
2025-08-23 22:57:49 -0400

    Initial commit

---
+++ src/SimpleSC.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract SimpleSC {
+    int256 public x;                 // stored integer
+    function add(int256 dx) external { x += dx; }  // x ← x + dx
+}
